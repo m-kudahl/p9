138e2994949b5d6c67d9fa7cbbc6d0277b9dc057
m-kudahl <mathias.laursen97@gmail.com>
2023-10-02 13:01:00 +0200

    Fix Update

--- p9-app/src/App.js
+++ p9-app/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          LOOOL
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          adawfawfawfawd
         </a>
       </header>
     </div>

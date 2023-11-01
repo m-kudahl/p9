2e6d16f7d1b3d1bf2d4cea0df174051fc540cdd6
m-kudahl <mathias.laursen97@gmail.com>
2023-11-01 18:51:59 +0100

    CSS

--- p9-app/src/TimeLeft.js
+++ p9-app/src/TimeLeft.js
@@ -3,7 +3,7 @@ import './TimeLeft.css';
 
 function TimeLeft() {
   return (
-    <div style={{ background: '#D9D9D9', padding: '10px', color: 'white', textAlign: 'center', width: "241px",
+    <div className='timeleft' style={{ background: '#D9D9D9', padding: '10px', color: 'white', textAlign: 'center', width: "241px",
     height: "150px", position: 'relative', left: "1000px", top: "150px"}}>
       <p>Time Left</p>
       <p>01:20:05</p>

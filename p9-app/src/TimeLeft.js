1f0c04a3344f7f76fb2de01ec16a8f304771b341
m-kudahl <mathias.laursen97@gmail.com>
2023-11-02 09:02:39 +0100

    Coding cleanup

--- p9-app/src/TimeLeft.js
+++ p9-app/src/TimeLeft.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './TimeLeft.css';
 
-function TimeLeft() {
+export default function TimeLeft() {
   return (
 
-    <div className='timeleft' style={{ background: '#D9D9D9', padding: '10px', color: 'white', textAlign: 'center', width: "241px",
-    height: "150px", position: 'relative', left: "1000px", top: "150px"}}>
+    <div className='timeleft'
+  style={{
+      background: '#D9D9D9',
+      padding: '10px',
+      color: 'white',
+      textAlign: 'center',
+      width: "241px",
+      height: "150px",
+      position: 'relative',
+      left: "1000px",
+      top: "150px"
+      }}>
         
       <p>Time Left</p>
       <p>01:20:05</p>
     </div>
   );
 }
-
-export default TimeLeft;

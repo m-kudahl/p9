c6874698d2ea292bd9630444266af8801d37a902
m-kudahl <115213053+m-kudahl@users.noreply.github.com>
2023-11-02 11:09:24 +0100

    Formatting

--- p9-app/src/TimeEstimated.js
+++ p9-app/src/TimeEstimated.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import './TimeEstimated.css'
+import React from "react";
+import "./TimeEstimated.css";
 
 export default function TimeEstimated() {
   return (
-    
-    <div className='estimate'>
+    <div className="estimate">
       <p>Time Estimated</p>
       <p>01:20:05</p>
     </div>
-  )
+  );
 }
--- p9-app/src/TimeLeft.js
+++ p9-app/src/TimeLeft.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import './TimeLeft.css';
+import React from "react";
+import "./TimeLeft.css";
 
 export default function TimeLeft() {
   return (
-
-    <div className='timeleft'>
+    <div className="timeleft">
       <p>Time Left</p>
       <p>01:20:05</p>
     </div>
-  )
+  );
 }

842df6d3e5aa6ad41b7a80a046e649b13be3ca5b
Veteranpit <115033980+Veteranpit@users.noreply.github.com>
2023-11-06 11:57:57 +0100

    Removed work from other branches

--- p9-app/src/App.js
+++ p9-app/src/App.js
@@ -1,34 +1,12 @@
-
-import MyButton from './MyButton';
 import Alertbox from './Alertbox';
 
 function App() {
 
-const Mybutton = ({ top, background, children}) =>{ 
-  const MyButton = { 
-    position: "absolute",
-    width: "241px",
-    height: "150px", 
-    left: "39px",
-    top: top,
-    border: "1px solid #000000", 
-    background: background, 
 
-
-};
-
-return <button style={MyButton}>{children}</button>;
-
-};
 
 return (
   <>
-    <Mybutton top="16px" background="#62BE59" > Start</Mybutton>
-    <Mybutton top="180px" background="#DB1414" > Stop</Mybutton>
-    <Mybutton top="345px" background="#FFFFFF" > Reset</Mybutton>
-    <Mybutton top="510px" background="#EEEEEE" > Pause </Mybutton>
-
-    <MyButton />
+    
     <Alertbox />
   
   </>
--- p9-app/src/MyButton.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function MyButton() {
-  return (
-    <div></div>
-  )
-}

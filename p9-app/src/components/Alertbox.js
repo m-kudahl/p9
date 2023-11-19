9747ad5a1765c66d47a23523985023f0650f8bfb
Veteranpit <115033980+Veteranpit@users.noreply.github.com>
2023-11-19 10:16:07 +0100

    alert json 0.5

--- p9-app/src/components/Alertbox.js
+++ p9-app/src/components/Alertbox.js
@@ -1,29 +1,44 @@
-import React from "react";
-import './Alertbox.css'
+import React, { useEffect, useState } from "react";
+import './Alertbox.css';
 
 export default function Alertbox() {
-  const openCustomPopup = () => {
-    // Create a new popup window
-    const popupWindow = window.open('', 'PopupWindow', 'width=400,height=200');
+  const [alertData, setAlertData] = useState([]);
 
-    // Write your custom message to the popup window
+  const openCustomPopup = (message) => {
+    const popupWindow = window.open('', 'PopupWindow', 'width=400,height=200');
     popupWindow.document.write('<html><head><title>Custom Popup</title></head><body>');
-    popupWindow.document.write('<h1>Placeholder for error message</h1>');
+    popupWindow.document.write(`<h1>${message}</h1>`);
     popupWindow.document.write('<button onclick="window.close();">Close</button>');
     popupWindow.document.write('</body></html>');
   };
+
+  useEffect(() => {
+    // Fetch alert data from the JSON file
+    fetch('/alerts.json')
+      .then((response) => response.json())
+      .then((data) => setAlertData(data))
+      .catch((error) => console.error('Error fetching alert data:', error));
+  }, []);
+
   return (
-        <div className="CenterBox"> 
-            <div className="AlertHeader">
-                ALERTS
-            </div>
-            <div className="AlertList">
-                <div className="AlertEntry" style={{background: 'red'}} onClick={openCustomPopup}> ERROR 7: PLACEHOLDER </div>
-                <div className="AlertEntry" style={{background: 'orange'}}onClick={openCustomPopup}>  WARNING: PLACEHOLDER </div>
-                <div className="AlertEntry" onClick={openCustomPopup}> Placeholder </div>
-            </div>
-
-        </div> 
-
-  )
+    <div className="CenterBox">
+      <div className="AlertHeader">
+        ALERTS
+      </div>
+      <table className="AlertList">
+        <tbody>
+          {alertData.map((alert, index) => (
+            <tr
+              key={index}
+              className="AlertEntry"
+              style={{ background: alert.type === 'error' ? 'red' : alert.type === 'warning' ? 'orange' : 'inherit' }}
+              onClick={() => openCustomPopup(alert.message)}
+            >
+              <td>{alert.message}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }

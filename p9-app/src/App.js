267eba78718a2736bf8af3d98c6884f898dd52f1
m-kudahl <115213053+m-kudahl@users.noreply.github.com>
2023-11-19 20:00:03 +0100

    Formatting

--- p9-app/src/App.js
+++ p9-app/src/App.js
@@ -9,7 +9,6 @@ return (
     <TimeLeft />
     <DoorStatus />
     
-  
   </>
 );
 };
--- p9-app/src/DoorStatus.js
+++ p9-app/src/DoorStatus.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./DoorStatus.css";
+
 import safetyData from "./safetyData.json";
 
 export default function TimeEstimated() {
   const doorStatus = safetyData.door.doorStatus;
+
   return (
     <div className="doorDiv">
       <h1>Door Status</h1>
+
       <p>{doorStatus}</p>
     </div>
   );

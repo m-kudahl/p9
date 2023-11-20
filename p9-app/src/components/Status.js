a397b153e47c0ad3d732c5fdbf57e00b70d2ee6c
m-kudahl <115213053+m-kudahl@users.noreply.github.com>
2023-11-20 10:15:03 +0100

    Status JSON

--- p9-app/src/components/Status.js
+++ p9-app/src/components/Status.js
@@ -1,19 +1,24 @@
-import "./Status.css"
+import "./Status.css";
 
+import Data from "./Data.json";
 
-export default function StatusBoxes () {
-    return ( 
-        <div className='Status'>
-            <div className='DoorStatus'>
-                <h2> DOOR STATUS</h2>
-                <div className='DoorLine'></div>
-                <h2> UNLOCKED </h2>
-            </div>
-            <div className='LaserStatus'>
-                <h2> LASER STATUS</h2>
-                <div className='LaserLine'></div>
-                <h2 className='Ready'> NOT READY</h2>
-            </div>
-        </div>
-     )
-  }
+export default function StatusBoxes() {
+  const doorStatus = Data.door.status;
+  const laserStatus = Data.laser.status;
+
+  return (
+    <div className="Status">
+      <div className="DoorStatus">
+        <h2>DOOR STATUS</h2>
+        <div className="DoorLine"></div>
+        <h2>{doorStatus}</h2>
+      </div>    
+      
+      <div className="LaserStatus">
+        <h2>LASER STATUS</h2>
+        <div className="LaserLine"></div>
+        <h2 className="Ready">{laserStatus}</h2>
+      </div>
+    </div>
+  );
+}

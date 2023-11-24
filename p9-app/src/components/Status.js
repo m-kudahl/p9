f9b0a80de918ca59290a74c2f05abdfe150f34bd
HiwotYitbarek <114922849+HiwotYitbarek@users.noreply.github.com>
2023-11-24 08:08:30 +0100

    h

--- p9-app/src/components/Status.js
+++ p9-app/src/components/Status.js
@@ -8,7 +8,29 @@ export default function StatusBoxes() {
 const [doorStatus, setdoorStatus] = useState("");
 const [laserStatus, setlaserStatus] = useState("");
 
+useEffect(() => {
 
+  fetch('https://raw.githubusercontent.com/m-kudahl/p9/Hiwot/p9-app/src/components/Data.json')
+  .then(response =>{
+    if(!response.ok) {
+    throw new Error(response.status);
+  }
+    return response.json();
+  })
+  .then ((data) => {
+  
+    console.log(data)
+    const doorStatuses = data.door.map(item => item.status);
+    const laserStatuses = data.laser.map(item => item.status);
+    console.log('Door Statuses', doorStatuses);
+    console.log('Laser Statuses', laserStatuses);
+
+  })
+  .catch(error => {
+    console.error('Error fetchinhg data', error);
+
+});
+});
 
   //const doorStatus = Data.door.status;
   //const laserStatus = Data.laser.status;
@@ -28,4 +50,4 @@ const [laserStatus, setlaserStatus] = useState("");
       </div>
     </div>
   );
-}
+  }

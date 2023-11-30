421be1a697603f5e34c72ca94da4edb593defd88
m-kudahl <115213053+m-kudahl@users.noreply.github.com>
2023-11-30 17:12:11 +0100

    START/PAUSE Basic

--- p9-app/src/components/Sidebar/Sidebar.js
+++ p9-app/src/components/Sidebar/Sidebar.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import './Sidebar.css';
+import StartButton from './StartButton'; // Import the new component
 import SidebarButtons from './SidebarButtons.js';
 import ChangeUser from './ChangeUser.js';
 
-  
 export default function Sidebar() {
   return (
- <div className='Sidebar' > 
-    <SidebarButtons top="5%" background="#62BE59" > START</SidebarButtons>
-    <SidebarButtons top="30%" background="#DB1414" > STOP</SidebarButtons>
-    <SidebarButtons top="55%" background="#FFFFFF" > RESET</SidebarButtons>
-    <ChangeUser> </ChangeUser>
+    <div className='Sidebar'>
+      <StartButton top="5%" background="#62BE59"> </StartButton>
+      <SidebarButtons top="30%" background="#DB1414"> STOP</SidebarButtons>
+      <SidebarButtons top="55%" background="#FFFFFF"> RESET</SidebarButtons>
+      <ChangeUser> </ChangeUser>
     </div>
-  )
+  );
 }
-
-

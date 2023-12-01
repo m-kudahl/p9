f538404e5945d1c088902cbf90fa6919f5476319
JonasTarp <115178984+bioe3@users.noreply.github.com>
2023-12-01 13:21:11 +0100

    Update App.js
    Jonas

--- p9-app/src/App.js
+++ p9-app/src/App.js
@@ -6,19 +6,23 @@ import Navbar from "./components/Navbar.js";
 import StatusBoxes from "./components/Status.js"
 import TimeLeft from './components/TimeLeft.js';
 import Alertbox from './components/Alertbox.js';
+import Popup from './components/Popup.js';
 
+function App(){
+  const [isPopupOpen, setPopupOpen] = useState(false);
+  const [selectedAlert, setSelectedAlert] = useState(null);
 
-
-
+  
 function App(){
 return <div className="App">
   <>
-  <Alertbox />
+  <Alertbox setPopupOpen={setPopupOpen} setSelectedAlert={setSelectedAlert} />
   <Sidebar/>
   <Image/>
   <Navbar/>
   <StatusBoxes/>
   <TimeLeft />
+  {isPopupOpen && <Popup setPopupOpen={setPopupOpen} selectedAlert={selectedAlert} />}
   </>
 </div>
 }

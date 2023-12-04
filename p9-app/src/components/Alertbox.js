4f0380b1671734d18618bed4e685da169c4e03aa
Veteranpit <115033980+Veteranpit@users.noreply.github.com>
2023-12-04 23:17:14 +0100

    JSON CHANGE

--- p9-app/src/components/Alertbox.js
+++ p9-app/src/components/Alertbox.js
@@ -16,22 +16,23 @@ export default function Alertbox() {
     setSelectedAlert(null); // Reset selectedAlert
   };
 
-  const handleAddAlert = async (type) => { // Function to add new alerts
-    try {  // We are using a try-catch here because inputting a line of text is (probably) an easy way to make errors (probably not needed before we implement functionality for the client to input new alerts in the browser instead of in code)
+  const handleAddAlert = async (type) => {
+    try {
       const response = await fetch('http://localhost:8080/api/alerts', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json', // This shows that the content is in a .json format
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ type: type, message: 'New Alert Message' }), // This is what is sent to the server in the body. Currently this is how we input new alerts. 
+        body: JSON.stringify({ type: type, message: 'New Alert Message' }),
       });
 
       if (!response.ok) {
         throw new Error('Failed to add new alert');
-      } 
-      const updatedData = await response.json();
-      
-      setAlertData(updatedData); // Update the state of alertData which visually adds the new alert to the table in the browser
+      }
+
+      const newAlert = await response.json();
+
+      setAlertData((prevData) => [...prevData, newAlert]);
 
     } catch (error) {
       console.error('Error adding alert:', error);
--- p9-app/src/components/Server.mjs
+++ p9-app/src/components/Server.mjs
@@ -1,84 +1,105 @@
-import express from 'express'; //express is a web framework for Node. We will use it to handle requests such as GET POST AND PUT
-import fetch from 'node-fetch'; // We need to import fetch from Node as this is a server and React is meant for clients
-import cors from 'cors'; //CORS is used to allow different websites to share and use resources. It is like crossplay on xbox and playstation :) 
+import express from 'express';
+import cors from 'cors';
+import path from 'path'; // Import the path module to work with file paths
+import fs from 'fs/promises'; 
 
-const app = express(); // Initialize an instance of express app. 
-const PORT = 8080; //It is convention to use port 8080 for web servers
+const app = express();
+const PORT = 8080;
 
 app.use(express.json());
-app.use(cors({  //  Here we setup the credentials for CORS. Without this, the alertbox running on port 3000 would not be able to access the json from this server running on port 8080
+app.use(cors({
   origin: 'http://localhost:3000',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 }));
 
+let alertData = [];
+let doorStatus = ' ';
 
-let alertData = []; //  Array to store alerts
+// Use the path module to create the absolute paths to your local JSON files
+const alertsFilePath = path.join('G:', 'p9', 'p9-app', 'public', 'alerts.json');
+const doorStatusFilePath = path.join('G:', 'p9', 'p9-app', 'public', 'door.json');
 
-// This is an endpoint. We use this one to get alerts from Github. Endpoints allow us to give a response (res) to a request (req). We are making an asynchronous (async) function, which allows us to use the await keyword. 
-// This basically lets the function pause while waiting for a fetch, as well as letting the program as a whole continue working while fetching instead of freezing
-app.get('/api/alerts', async (req, res) => {    
+app.get('/api/alerts', async (req, res) => {
   try {
-    const response = await fetch('https://raw.githubusercontent.com/m-kudahl/p9/mathias2/p9-app/public/alerts.json');
-    const data = await response.json();
-    alertData = data; // import data from github into alertData array
-    res.json(data); // Responds to the request with data array
-    //res.status(200).json({ message: 'Alerts fetched from Github'}) // Tags response with a HTTP status code. 2xx = good 4xx = bad request 5xx = internal server error
+    const data = await fs.readFile(alertsFilePath, 'utf-8');
+    const dataArray = JSON.parse(data);
+
+    alertData = dataArray;
+    res.json(dataArray);
   } catch (error) {
-    console.error('Error fetching alerts from GitHub:', error);
+    console.error('Error fetching alerts locally:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-
-// Endpoint to update alerts
 app.put('/api/alerts', async (req, res) => {
-  const updatedData = req.body; // Get data from the request and put it in variable updatedData
+  const updatedData = req.body;
 
   try {
+    // Read the existing alerts data from the file
+    const currentData = await fs.readFile(alertsFilePath, 'utf-8');
+    const currentAlerts = JSON.parse(currentData);
 
-    alertData = updatedData; // Update the in-memory data
-    res.json(updatedData); // Respond with the updated data
-    //res.status(201).json({ message: 'Updated alerts sent'})
+    // Append the new data to the existing data
+    const newData = [...currentAlerts, ...updatedData];
 
+    // Update the alerts.json file with the new data
+    await fs.writeFile(alertsFilePath, JSON.stringify(newData, null, 2), 'utf-8');
+
+    // Do not update alertData here
+    // alertData = newData;
+
+    res.json(newData);
+    console.log('Server updated alerts and sent response');
   } catch (error) {
     console.error('Error updating alerts on the server:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
+
+
+
 // Endpoint to update door status
 app.put('/api/doorstatus', async (req, res) => {
   const updatedData = req.body;
   console.log('Received data:', updatedData);
 
-  if (updatedData.doorStatus === 'locked') {
-    res.json({ doorStatus: 'unlocked' }); // set door status to unlocked
-    console.log('Server set data to unlocked');
-  } else if (updatedData.doorStatus === 'unlocked') {
-    res.json({ doorStatus: 'locked' }); // set door status to locked
-    console.log('Server set data to locked');
-  } else {
-    res.status(400).json({ error: 'Invalid door status' });
+  try {
+    let newStatus;
+
+    if (updatedData.doorStatus === 'locked') {
+      newStatus = 'unlocked';
+      console.log('Server set data to unlocked');
+    } else if (updatedData.doorStatus === 'unlocked') {
+      newStatus = 'locked';
+      console.log('Server set data to locked');
+    } else {
+      return res.status(400).json({ error: 'Invalid door status' });
+    }
+
+    // Update the door.json file with the new status
+    const updatedStatus = { door: { status: newStatus } };
+    await fs.writeFile(doorStatusFilePath, JSON.stringify(updatedStatus, null, 2), 'utf-8');
+
+    res.json({ doorStatus: newStatus });
+    console.log('Server updated door status and sent response');
+  } catch (error) {
+    console.error('Error updating door status on the server:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-
-
-
-
-
-
-// Endpoint to add alerts
-app.post('/api/alerts', (req, res) => {
-  const newAlert = req.body; // Create new alert using input from the request
+app.post('/api/alerts', async (req, res) => {
+  const newAlert = req.body;
 
   try {
-
-    // Add the new alert to the in-memory data (updates active alert data with new alert)
     alertData.push(newAlert);
 
-    // Respond with the updated data
+    // Update the alerts.json file with the new data
+    await fs.writeFile(alertsFilePath, JSON.stringify(alertData, null, 2), 'utf-8');
+
     res.json(alertData);
-    //res.status(202).json({ message: 'New alert added'})
+    console.log('Server updated alerts and sent response');
   } catch (error) {
     console.error('Error adding alert on the server:', error);
     res.status(500).json({ error: 'Internal Server Error' });
@@ -86,18 +107,16 @@ app.post('/api/alerts', (req, res) => {
 });
 
 
-let doorStatus = ' '; // Variable to store doorstatus
-// Endpoint to get door status
 app.get('/api/doorstatus', async (req, res) => {
   try {
-    const response = await fetch('https://raw.githubusercontent.com/m-kudahl/p9/7881f92ccd8a1e26221f0360839662ab8ef7d376/p9-app/public/door.json');
-    const data = await response.json();
-    doorStatus = data.door.status; // import status of door from github into doorStatus variable
-    res.json(doorStatus) // responds to the request with data variable
-  }
-  catch (error) {
-    console.error('Error fetching doorstatus from GitHub', error);
-    res.status(500).json({ error: 'Internal Server Error' })
+    const data = await fs.readFile(doorStatusFilePath, 'utf-8');
+    const doorStatusData = JSON.parse(data);
+
+    doorStatus = doorStatusData.door.status;
+    res.json(doorStatus);
+  } catch (error) {
+    console.error('Error fetching door status locally:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
